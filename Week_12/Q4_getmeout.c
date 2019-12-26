41802d147f078396dbb2bde6d03f813ffb7f9ffb
suyunu <buraksuyunu@gmail.com>
2019-12-26 22:01:51 +0300

    string question fix

--- Week_12/Q4_getmeout.c
+++ Week_12/Q4_getmeout.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 
+void normalizeString(char s[]){
+	if(s[strlen(s)-2] == '\r' || s[strlen(s)-2] == '\n')
+		s[strlen(s)-2] = '\0';
+	else if(s[strlen(s)-1] == '\r' || s[strlen(s)-1] == '\n')
+		s[strlen(s)-1] = '\0';
+}
+
 int main()
 {
 	int i;
 	char s1[100], s2[100];
-	gets(s1);
-	gets(s2);
+	fgets(s1,100,stdin);
+	fgets(s2,100,stdin);
+	
+	// This part ensures that strings do not have \n or \r at the end before \0.
+	normalizeString(s1);
+	normalizeString(s2);
 
 	for(i = 0 ; i < strlen(s1) ; i++){
 		if(strncmp(s1+i, s2, strlen(s2)) == 0){
-		    char temp[100];
-		    strcpy(temp, s1+i+strlen(s2)+1);
+			char temp[100];
+			strcpy(temp, s1+i+strlen(s2)+1);
 			strcpy(s1+i, temp);
 			break;
 		}
